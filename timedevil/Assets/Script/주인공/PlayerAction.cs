384c41f8e15544694ebd1de426dedd9379af19db
jun3181 <rlatjdwns0109@gmail.com>
2025-07-07 20:47:54 +0900

    feature/MoveMent

---
+++ timedevil/Assets/Script/주인공/PlayerAction.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    float h;
+    float v;
+    bool isHorizonMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }
+        else if (hUp || vUp)
+        {
+            isHorizonMove = h != 0;
+        }
+
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v).normalized * Speed;
+        Vector2 moveVec = new Vector2(h, v).normalized;
+    }
+}
