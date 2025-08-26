594469a6ed79ccea3f2745da2b2160d32a8ab867
HWANCHUL YU <53122601+Cfghle@users.noreply.github.com>
2025-08-26 12:17:14 +0900

    디자인 추가

--- Web/kyweb/src/Downloadpage.jsx
+++ Web/kyweb/src/Downloadpage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import './Downloadpage.css';
 
 
 
 export default function DownloadPage(){
   return(
     <div>
-      <img src = {logo} style = {{top: "50px"}}></img>
+      <div className = "backgroundimg"></div>
       <h1>게임설명</h1>
-      <button>다운로드</button>
+      <button className = "download-button">다운로드</button>
     </div>
   )
 }
--- Web/kyweb/src/Homepage.jsx
+++ Web/kyweb/src/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState} from 'react';
 import logo from './logo.svg';
-
+import './Homepage.css';
 function Comment(){
   let [comments, setComments] = useState([
     {id: 0, name: "홍길동", content: "안녕하세요!" },
@@ -20,10 +20,10 @@ function Comment(){
         </p>
       </form>
 
-      <div>
-        {comments.map((comment) =>{
+      <div>{/** 댓글 목록 */}
+        {comments.slice().reverse().map((comment) =>{
           return(
-            <div>
+            <div className = "comment">
               <p>{comment.name} : {comment.content}</p>
             </div>
           )
@@ -40,10 +40,10 @@ export default function HomePage() {
 
     return(
         <div>
-          <img src = {logo}></img>
-          <h2>타임 데빌</h2>
+          <div className = "backgroundimg"><h2>타임 데빌</h2></div>
+          
           <h3>게임 설명</h3>
-          <p>1. '테스트용'이라는 용어는 주로 소프트웨어 개발 및 품질 보증 과정에서 사용됩니다. 이는 특정 기능이나 시스템이 제대로 작동하는지 확인하기 위한 테스트를 수행하는 것을 의미합니다. 개발자와 QA 엔지니어는 다양한 테스트를 통해 코드의 안정성, 성능, 보안성을 검토하며, 사용자 경험을 개선하기 위해 피드백을 받습니다. 이 과정에서 '테스트용' 데이터나 환경이 필요하며, 실제 사용자 정보와는 분리되어 있어야 합니다. 이러한 테스트는 최종 제품이 사용자의 기대에 부응할 수 있도록 보장하는 중요한 단계입니다.</p>
+          <p className = "description">1. '테스트용'이라는 용어는 주로 소프트웨어 개발 및 품질 보증 과정에서 사용됩니다. 이는 특정 기능이나 시스템이 제대로 작동하는지 확인하기 위한 테스트를 수행하는 것을 의미합니다. 개발자와 QA 엔지니어는 다양한 테스트를 통해 코드의 안정성, 성능, 보안성을 검토하며, 사용자 경험을 개선하기 위해 피드백을 받습니다. 이 과정에서 '테스트용' 데이터나 환경이 필요하며, 실제 사용자 정보와는 분리되어 있어야 합니다. 이러한 테스트는 최종 제품이 사용자의 기대에 부응할 수 있도록 보장하는 중요한 단계입니다.</p>
           <Comment></Comment>
         </div>
     )
